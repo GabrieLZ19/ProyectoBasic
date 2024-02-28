f40b3dfa88e420afafa3abb32e6e2d6164bfc52e
Gabriel Lazo <gabriellazo48@gmail.com>
2024-02-28 12:05:33 -0300

    Cambios en Homework II

--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,55 @@
-class Activity{
-
-    constructor(id,title,description,imgUrl){
-
-        this.id=id;
-        this.title=title;
-        this.description=description;
-        this.imgUrl=imgUrl;
-
-    }
-
-
+class Activity {
+  constructor(id, title, description, imgUrl) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.imgUrl = imgUrl;
+  }
 }
 
-class Repository{
+class Repository {
+  constructor() {
+    this.activities = [];
+    this.id = 0;
+  }
+  getAllActivities() {
+    return this.activities;
+  }
 
-    constructor(){
+  createActivity(title, description, imgUrl) {
+    this.id++;
+    const activity = new Activity(this.id, title, description, imgUrl);
+    this.activities.push(activity);
+  }
 
-        this.activities=[];
-    }
-    getAllActivities(){
-        
-        return this.activities;
-        
-    }
-
-    createActivity(arreglo){
-
-        this.activities.push(arreglo);
-
-    }
-
-    deleteActivity(id){
-
-        this.activities=this.activities.filter((item)=> item.id!==id);
-    }
+  deleteActivity(id) {
+    this.activities = this.activities.filter((item) => item.id !== id);
+  }
 }
+
+const prueba = new Repository();
+
+console.log(prueba.createActivity("yoga", "es bueno", "#"));
+console.log(prueba.getAllActivities());
 
 const formulario = document.querySelector("#contenedorActividades");
 
-formulario.addEventListener("submit",validarFormulario);
+formulario.addEventListener("submit", validarFormulario);
 
-function validarFormulario(elemento){
+function validarFormulario(elemento) {
+  elemento.preventDefault(); //no recarga la pagina
 
-    elemento.preventDefault(); //no recarga la pagina
+  let actividad = document.querySelector("#actividad").value;
+  let descripcion = document.querySelector("#descripcion").value;
+  let link = document.querySelector("#link").value;
 
-    let actividad = document.querySelector("#actividad").value;
-    let descripcion = document.querySelector("#descripcion").value;
-    let link= document.querySelector("#link").value;
+  let titulo = document.querySelector("#tituloActividad");
+  let comentario = document.querySelector("#descripcionActividad");
+  let imagen = document.querySelector("#imagenActividad");
 
-    let titulo= document.querySelector("#tituloActividad");
-    let comentario= document.querySelector("#descripcionActividad");
-    let imagen= document.querySelector("#imagenActividad")
+  titulo.textContent = actividad;
+  comentario.textContent = descripcion;
+  imagen.src = link;
 
-    titulo.textContent= actividad;
-    comentario.textContent=descripcion;
-    imagen.src=link;
-    
-    formulario.reset();
-    
+  formulario.reset();
 }
